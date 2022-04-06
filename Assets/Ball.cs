b7e68fe00f097a0e50f0b4a9550af65ecd904181
Thanagorn16 <thanagorn1611@gmail.com>
2022-04-06 15:05:49 +0700

    make bouncing ball (done)

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -9,9 +9,9 @@ public class Ball : MonoBehaviour
     Vector2 rawInput;
     bool getClick = false;
     Rigidbody2D rb;
+    Vector3 lastVelocity;
     [SerializeField] float moveSpeed = 3;
-    // [SerializeField] float moveSpeedx = 3;
-    // [SerializeField] float moveSpeedy = 3;
+
 
     void Awake()
     {
@@ -29,25 +29,6 @@ public class Ball : MonoBehaviour
         GetAngle();
     }
 
-    void Shoot()
-    {
-        if(getClick)
-        {
-            rb.velocity = transform.up * moveSpeed; // shoot the ball with velocity from Rigidbody2D.
-            // Debug.Log("speed: " + rb.velocity);
-            // Debug.Log("11111111111111");
-            // Vector2 vel = rb.velocity;
-            // rb.velocity = transform.up * new Vector2(Mathf.Clamp(vel.x, -(moveSpeedx), moveSpeedx), Mathf.Clamp(vel.y, -(moveSpeedy), moveSpeedy));
-            // Debug.Log("222222222");
-            Debug.Log("rb: " + rb.velocity);
-            Debug.Log(this.rb.velocity);
-        }
-    }
-
-    void OnClick(InputValue value)
-    {
-        getClick = value.isPressed;
-    }
     void GetAngle()
     {
         Vector3 mousePostion = Camera.main.ScreenToWorldPoint(navigator.GetMousePosition()); //get mouse position 
@@ -55,6 +36,30 @@ public class Ball : MonoBehaviour
         float angle = Vector2.SignedAngle(Vector2.up, direction); // find the angle between Vector2.up and the direction of the mouse from the object
         transform.eulerAngles = new Vector3(0,0,angle); // rotate the object by setting the object’s Z rotation to the angle value
     }
+
+    void Shoot()
+    {
+        if(getClick)
+        {
+            rb.velocity = transform.up * moveSpeed; // shoot the ball with velocity from Rigidbody2D.
+        }
+
+        lastVelocity = rb.velocity; // get the last veloxity from before bouncing to then give it to the ball after bounce
+    }
+    void OnClick(InputValue value)
+    {
+        getClick = value.isPressed;
+    }
+
+    void OnCollisionEnter2D(Collision2D other) // the entire method make the ball bounce properly
+    {
+        float speed = lastVelocity.magnitude;
+        Vector3 direction = Vector3.Reflect(lastVelocity.normalized, other.contacts[0].normal);
+
+        rb.velocity = direction * Mathf.Max(speed, 0f);
+    }
+
+
     // void StickToMosue()
     // {
     //     Vector2 mousePostion = Camera.main.ScreenToWorldPoint(navigator.GetMousePosition());
