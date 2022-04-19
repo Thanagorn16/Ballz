e001bfec9f1a9115781a7f968f456bd6851c9441
Thanagorn16 <thanagorn1611@gmail.com>
2022-04-19 12:28:10 +0700

    fail to instantiate the ball after getting the item / this project is suspended unitl I figure out a way to fix it

--- Assets/Scripts/CloneProperty.cs
+++ Assets/Scripts/CloneProperty.cs
@@ -3,18 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class Ball : MonoBehaviour
+public class CloneProperty : MonoBehaviour
 {
     Navigator navigator;
     CloneManagement cloneManager;
     Vector2 rawInput;
-    bool getClick = false;
+    // bool getClick = false;
     Rigidbody2D rb;
     Vector3 lastVelocity;
     CreateClone createClone;
     [SerializeField] float moveSpeed = 3;
     // [SerializeField] int clone = 0;
-    // [SerializeField] bool isClone;
+    [SerializeField] bool isClone;
     // [SerializeField] GameObject ballPrefab;
 
     void Awake()
@@ -32,52 +32,51 @@ public class Ball : MonoBehaviour
     void Update()
     {
         Shoot();
-        GetAngle();
+        // GetAngle();
     }
 
-    void GetAngle()
-    {
-        Vector3 mousePostion = Camera.main.ScreenToWorldPoint(navigator.GetMousePosition()); //get mouse position 
-        Vector2 direction = mousePostion - transform.position; // getting a direction works by subtracting the starting position from the target.
-        float angle = Vector2.SignedAngle(Vector2.up, direction); // find the angle between Vector2.up and the direction of the mouse from the object
-        transform.eulerAngles = new Vector3(0,0,angle); // rotate the object by setting the object’s Z rotation to the angle value
-    }
+    // void GetAngle()
+    // {
+    //     Vector3 mousePostion = Camera.main.ScreenToWorldPoint(navigator.GetMousePosition()); //get mouse position 
+    //     Vector2 direction = mousePostion - transform.position; // getting a direction works by subtracting the starting position from the target.
+    //     float angle = Vector2.SignedAngle(Vector2.up, direction); // find the angle between Vector2.up and the direction of the mouse from the object
+    //     transform.eulerAngles = new Vector3(0,0,angle); // rotate the object by setting the object’s Z rotation to the angle value
+    // }
 
     void Shoot()
     {
-        if(getClick)
+        // rb.velocity = transform.up * moveSpeed; // shoot the ball with velocity from Rigidbody2D.
+        if(isClone)
         {
             rb.velocity = transform.up * moveSpeed; // shoot the ball with velocity from Rigidbody2D.
         }
 
-        // for(int i = 0; i < cloneManager.GetClone(); i++)
-        // {
-        //     // Debug.Log("clone" + i);
-        //     // GameObject instaObj = Instantiate(ballPrefab, transform.position, Quaternion.identity);
-        // }
+        // // for(int i = 0; i < cloneManager.GetClone(); i++)
+        // // {
+        // //     // Debug.Log("clone" + i);
+        // //     // GameObject instaObj = Instantiate(ballPrefab, transform.position, Quaternion.identity);
+        // // }
 
         lastVelocity = rb.velocity; // get the last veloxity from before bouncing to then give it to the ball after bounce
-        // Debug.Log("last: " + lastVelocity);
-
+        // Debug.Log("shoot");
     }
 
-    void OnClick(InputValue value)
-    {
-        getClick = value.isPressed;
-    }
+    // void OnClick(InputValue value)
+    // {
+    //     getClick = value.isPressed;
+    // }
 
     void OnCollisionEnter2D(Collision2D other) 
     {
         if(other.gameObject.tag != "Bottom")
         {
             Bounce(other);
-            Debug.Log("collision");
-            Debug.Log("last: " + lastVelocity);
+            // Debug.Log("bounce");
         }
         else 
         {
             rb.velocity =  Vector2.zero;// stop every motion once hit the bottom
-            createClone.CreateNewClone(); //create new clone following the amount in CloneManagement once the ball hit the bottom.
+            // createClone.CreateNewClone(); //create new clone following the amount in CloneManagement once the ball hit the bottom.
         }
 
     }
@@ -99,7 +98,6 @@ public class Ball : MonoBehaviour
 
     void Bounce(Collision2D other) //the entire method make the ball bounce properly
     {
-        // Debug.Log("clone is here");
         float speed = lastVelocity.magnitude;
         Vector3 direction = Vector3.Reflect(lastVelocity.normalized, other.contacts[0].normal);
 
--- Assets/Scripts/CreateClone.cs
+++ Assets/Scripts/CreateClone.cs
@@ -5,18 +5,38 @@ using UnityEngine;
 public class CreateClone : MonoBehaviour
 {
     CloneManagement cloneManager;
+    Rigidbody2D rb;
+    Vector3 lastVelocity;
     [SerializeField] GameObject clonePrefab;
+    // [SerializeField] float moveSpeed = 3f;
 
     void Awake()
     {
         cloneManager = FindObjectOfType<CloneManagement>();
     }
 
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
     public void CreateNewClone()
     {
         for(int i = 0; i < cloneManager.GetClone(); i++)
         {
-            Instantiate(clonePrefab, transform.position, Quaternion.identity);
+            GameObject instaObject = Instantiate(clonePrefab, transform.position, Quaternion.identity);
+            // instaObject.transform.SetParent(gameObject.transform);
         }       
+
+        // rb.velocity = transform.up * moveSpeed; // shoot the ball with velocity from Rigidbody2D.
+        // lastVelocity = rb.velocity; // get the last veloxity from before bouncing to then give it to the ball after bounce
     }
+
+    // void Bounce(Collision2D other) //the entire method make the ball bounce properly
+    // {
+    //     float speed = lastVelocity.magnitude;
+    //     Vector3 direction = Vector3.Reflect(lastVelocity.normalized, other.contacts[0].normal);
+
+    //     rb.velocity = direction * Mathf.Max(speed, 0f);
+    // }
 }
